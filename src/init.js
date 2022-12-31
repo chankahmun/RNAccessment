03380e80ef3fef3636d6334877bea29549281258
ekchoo <ekchoo@intech2.com>
2022-12-31 18:14:11 +0800

    Project added

---
+++ src/init.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native'
+
+
+class Init extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        
+        return <View><Text>test</Text></View>
+    }
+
+}
+
+export default Init;
