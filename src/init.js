fd31342dcec6e1e3b97650c3b15e13391acaaf11
ekchoo <ekchoo@intech2.com>
2022-12-31 20:30:17 +0800

    Project added

--- src/init.js
+++ src/init.js
@@ -1,18 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native'
+import NavMainStack from './navigation/navMainStack';
 
-
-class Init extends Component{
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-
-        
-        return <View><Text>test</Text></View>
-    }
-
-}
+const Init =()=> <NavMainStack/>
 
 export default Init;
